931b53c049900a7836e0f7568d571390af34239d
박세훈 <sehoon@bagsehun-ui-MacBookPro.local>
2019-04-29 12:14:45 +0900

    All of the file has updated

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-var express = require('express');
-var app = express();
-app.use(express.static('public'));
+// var express = require('express');
+// var app = express();
+// app.use(express.static('public'));
+//
+// app.get('/',function(req,res){
+//    res.sendfile('index.html');
+// });
+//
+// app.listen(3000,function(){
+//    console.log('server running on 3000 port');
+// });
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-app.get('/',function(req,res){
-   res.sendfile('index.html');
+app.get('/', function(req, res){
+   res.send('<h1>Hello world</h1>');
 });
 
-app.listen(3000,function(){
-   console.log('server running on 3000 port');
+io.on('connection', function (socket) {
+   console.log('client '+ socket.id + ' connected');
+   socket.on('player', function (data) {
+      data.socketid = socket.id;
+      socket.broadcast.emit('player', data);
+   });
+   socket.on('disconnect', function () {
+      console.log('client ' + socket.id + ' disconnected');
+      socket.broadcast.emit('offline', {socketid: socket.id});
+   })
+});
+
+http.listen(3000, function(){
+   console.log('listening on *:3000');
 });
